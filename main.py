d662f4bf4b267689cb03ad43c6db7345985ba68d
Zen Alexey <alexeyzen2008@gmail.com>
2025-06-01 19:22:13 +0700

    command start is made

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Импорт библиотек
-import aiogram, asyncio, random, config
+import aiogram, asyncio, random, config, time
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command, CommandStart
@@ -11,6 +11,17 @@ from aiogram.fsm.state import State, StatesGroup
 # Создать бота
 bot = Bot(config.TOKEN)
 dp = Dispatcher()
+
+
+
+# Команда start
+@dp.message(CommandStart())
+async def command_start(message: Message):
+    button_add_word = InlineKeyboardButton(text='Добавить', callback_data='add_word')
+    button_get_word = InlineKeyboardButton(text='Получить', callback_data='get_word')
+    button = InlineKeyboardMarkup(inline_keyboard=[[button_add_word, button_get_word]])
+    await message.answer('Привет, я помогу тебе с запоминанием английский слов!\nПиши мне проблематичные слова и их перевод, я их буду сохранять и периодически,\
+                         а также когда ты меня попросишь высылать их тебе и запрашивать перевод.', reply_markup=button)
 
 
 
