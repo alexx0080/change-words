c196d22e54ac8eab9d0dd1f8ed541c3ee396b4c7
Zen Alexey <alexeyzen2008@gmail.com>
2025-06-01 19:40:22 +0700

    add function and button for add couple as english word and translate

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # Импорт библиотек
-import aiogram, asyncio, random, config, time
+import aiogram, asyncio, random, config, time, table_with_words
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+
+
+
+# Создать объект класса TableWord
+word_obj = table_with_words.TableWords()
+
+
+
+# Создать машину состояний для добавления слова и перевода
+class AddWord(StatesGroup):
+    get_eng_word = State()
+    get_translate = State()
 
 
 
@@ -20,9 +32,47 @@ async def command_start(message: Message):
     button_add_word = InlineKeyboardButton(text='Добавить', callback_data='add_word')
     button_get_word = InlineKeyboardButton(text='Получить', callback_data='get_word')
     button = InlineKeyboardMarkup(inline_keyboard=[[button_add_word, button_get_word]])
-    await message.answer('Привет, я помогу тебе с запоминанием английский слов!\nПиши мне проблематичные слова и их перевод, я их буду сохранять и периодически,\
+    await message.answer('Привет, я помогу тебе с запоминанием Английский слов!\nПиши мне проблематичные слова и их перевод, я их буду сохранять и периодически,\
                          а также когда ты меня попросишь высылать их тебе и запрашивать перевод.', reply_markup=button)
+    
 
+# Команда добавить слово + кнопка
+@dp.message(Command('add_word'))
+async def add_word_func(message: Message, state: FSMContext):
+    await message.answer('Введите Английское слово')
+    await state.set_state(AddWord.get_eng_word)
+
+@dp.message(AddWord.get_eng_word)
+async def get_english_word(message: Message, state: FSMContext):
+    eng_word = message.text
+    if eng_word.isalpha():
+        eng_word = eng_word.lower()
+        await state.update_data(english_word = eng_word)
+        await message.answer('Введите перевод')
+        await state.set_state(AddWord.get_translate)
+    else:
+        await message.answer('Введите корректное слово, состоящее только из букв')
+
+@dp.message(AddWord.get_translate)
+async def get_translate(message: Message, state: FSMContext):
+    trans = message.text
+    if trans.isalpha():
+        trans = trans.lower()
+        await state.update_data(translate = trans)
+        dictionary = await state.get_data()
+        eng_word = dictionary['english_word']
+        rus_word = dictionary['translate']
+        await state.clear()
+        word_obj.add_notice(eng_word, rus_word)
+        await message.answer('Отлично, слово добавлено в базу данных!')
+    else:
+        await message.answer('Введите корректный перевод, состоящий только из букв')
+
+@dp.callback_query(F.data == 'add_word')
+async def add_word_button(callback: CallbackQuery, state: FSMContext):
+    await add_word_func(callback.message, state)
+
+        
 
 
 # Запуск
